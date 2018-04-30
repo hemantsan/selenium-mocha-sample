c2c40c7f85f4eefd72e2c64ccf3fdf216cb3d2e0
Hemant Sankhla <sankhlahemant251@gmail.com>
2018-04-30 18:34:09 +0530

    code improved in chrome and firefox test files

--- __test__/chrome-test.js
+++ __test__/chrome-test.js
@@ -5,37 +5,20 @@ describe('Google Test', function() {
     var driver;
 
     before(function() {
-        console.log('before START');
-
         driver = new webdriver.Builder().forBrowser('chrome').build();
-
-        console.log('before END');
     });
 
     it('should load url', function(done) {
-        console.log('it START');
-
         driver
-            .get(
-                'https://blog.testproject.io/2017/06/07/javascript-testing-with-selenium-webdriver-mocha/'
-            )
-            .then(() => done());
-
-        driver
-            .getTitle()
-            .then(function(title) {
-                console.log('\n' + title);
+            .get('https://github.com/vieckys/selenium-mocha-sample')
+            .then(() => driver.getTitle())
+            .then(title => {
+                console.log('\n Page Title is : ' + title);
             })
             .then(() => done());
-
-        console.log('it END');
     });
 
     after(function() {
-        console.log('after START');
-
         driver.quit();
-
-        console.log('after END');
     });
 });
--- __test__/firefox-test.js
+++ __test__/firefox-test.js
@@ -1,41 +1,24 @@
 var webdriver = require('selenium-webdriver');
 var assert = require('assert');
 
-describe('Firefox Test', function() {
+describe('Google Test', function() {
     var driver;
 
     before(function() {
-        console.log('before START');
-
         driver = new webdriver.Builder().forBrowser('firefox').build();
-
-        console.log('before END');
     });
 
     it('should load url', function(done) {
-        console.log('it START');
-
         driver
-            .get(
-                'https://blog.testproject.io/2017/06/07/javascript-testing-with-selenium-webdriver-mocha/'
-            )
-            .then(() => done());
-
-        driver
-            .getTitle()
-            .then(function(title) {
-                console.log('\n' + title);
+            .get('https://github.com/vieckys/selenium-mocha-sample')
+            .then(() => driver.getTitle())
+            .then(title => {
+                console.log('\n Page Title is : ' + title + '\n');
             })
             .then(() => done());
-
-        console.log('it END');
     });
 
     after(function() {
-        console.log('after START');
-
         driver.quit();
-
-        console.log('after END');
     });
 });
